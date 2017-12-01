2e7cc166e7e2c50b13f20fef48257833d9e4384c
travisf <travisf@jfrog.com>
2017-12-01 13:58:45 -0800

    added anonymous to do-not-delete list

--- security/securityReplication/test/artifactory/SecurityTestApi.groovy
+++ security/securityReplication/test/artifactory/SecurityTestApi.groovy
@@ -83,7 +83,7 @@ class SecurityTestApi {
     static def deleteAllUsers () {
         Collection<String> userNames = art.security().userNames()
         for (String userName : userNames) {
-            if (userName != "admin" && userName != "access-admin") {
+            if (userName != "anonymous" && userName != "admin" && userName != "access-admin") {
                 art.security().deleteUser(userName)
             }
         }
